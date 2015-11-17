2d94cb27836028d65811469747744c640f9c2902
enzomartinez <enzo11101@hotmail.com>
2015-11-17 16:53:16 -0300

    Player

---
+++ player/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+    public float maxSpeed = 5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 pos = transform.position;
+
+        pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
+
+        pos.x += Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
+
+        transform.position = pos;
+
+    }
+}
