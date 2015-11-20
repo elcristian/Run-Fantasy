05516a1440b3d021fe476586ae936a51cd59163f
elcristian <elcristian00@hotmail.com>
2015-11-20 01:46:35 -0300

    Menu
    Agrege una Scenes menu la cual te pasa del menu principal ala Scenes1
    tambien funciona el boton exit que sierra la aplicasion

---
+++ Assets/Scripts/SciptsMenu/MenuInicial.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuInicial : MonoBehaviour {
+    public void Play()
+    {
+        Application.LoadLevel("Scenes1");
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
