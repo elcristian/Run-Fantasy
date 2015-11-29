0d362ed036ad2ba477ec7820fe12b061c75113ad
elcristian <elcristian00@hotmail.com>
2015-11-29 19:01:43 -0300

    Menu de inicial y de pausa
    ya quedaron los menus de pasua y de inicio 100% funcionando

---
+++ Assets/Scripts/SciptsMenu/Menu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void OnGUI(){
+		const int buttonWidth = 84;
+		const int buttonHeight = 60;
+		
+		// Determine the button's place on screen
+		// Center in X, 2/3 of the height in Y
+		Rect buttonRect = new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 3) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			);
+		
+		// Draw a button to start the game
+		if(GUI.Button(buttonRect,"Start!"))
+		{
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			Application.LoadLevel("Scenes1");
+		}
+        if (GUI.Button(buttonRect, "Exit"))
+        {
+            // On Click, load the first level.
+            // "Stage1" is the name of the first scene we created.
+            #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
+        }
+    }
+}
--- Assets/Scripts/SciptsMenu/MenuInicial.cs
+++ Assets/Scripts/SciptsMenu/MenuInicial.cs
@@ -1,13 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuInicial : MonoBehaviour {
     public void Play()
     {
         Application.LoadLevel("Scenes1");
     }
+    public void Credits()
+    {
+        Application.LoadLevel("Credits");
+    }
     public void Salir()
     {
+        #if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+        #else
         Application.Quit();
+        #endif
     }
 }
